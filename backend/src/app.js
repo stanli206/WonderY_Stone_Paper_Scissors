2fa77270e4d8b3eeb553df67041e69eb4f874347
stantlin s <stantilin@gmail.com>
2026-01-04 00:21:44 +0530

    `Update origin in CORS configuration`

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:
+      "http://wondery-stone-paper-scissors-ui.s3-website.eu-north-1.amazonaws.com", //http://localhost:5173
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
