e0739cf7095e0af69849e78c0a450a20e48992e6
stantlin s <stantilin@gmail.com>
2026-01-08 14:37:15 +0530

    `Update API endpoint URLs in Game and History components`

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -69,13 +69,16 @@ export default function Game() {
 
     setFinalWinner(winner);
 
-    await axios.post("http://16.171.148.112:5000/api/games", {
-      player1Name,
-      player2Name,
-      rounds,
-    });
+    await axios.post(
+      "http://wondery-backend-alb-763939103.eu-north-1.elb.amazonaws.com/api/games",
+      {
+        player1Name,
+        player2Name,
+        rounds,
+      }
+    );
   };
-//https://d3ilpfd9bcjuxp.cloudfront.net/api/games  
+  //https://d3ilpfd9bcjuxp.cloudfront.net/api/games
 
   const restartGame = () => {
     setRounds([]);
--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -7,7 +7,7 @@ export default function History() {
   useEffect(() => {
     axios
       .get(
-        "http://16.171.148.112:5000/api/games"
+        "http://wondery-backend-alb-763939103.eu-north-1.elb.amazonaws.com/api/games"
       ) // http://16.171.148.112:5000 , http://wondery-backend-alb-763939103.eu-north-1.elb.amazonaws.com/
       .then((res) => setGames(res.data));
   }, []);
