b79a9475cb94e1e2698d46e5ed721124643465aa
stantlin s <stantilin@gmail.com>
2026-01-11 23:50:26 +0530

    `Added Sequelize configuration with environment variables and SSL options`

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,22 @@
-import { Sequelize } from "sequelize";
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
+
+// export default sequelize;
 import dotenv from "dotenv";
 dotenv.config();
+import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -8,8 +24,15 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: 5432,
     dialect: "postgres",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
